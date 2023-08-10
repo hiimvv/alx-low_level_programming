5b1572243857818158af4a3744df8b36f5452e39
hiimvv <hemo.hhh71@gmail.com>
2023-08-10 15:33:51 -0700

    maybe?

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
-* main - a script for a qoute
+* main - A script for a qoute
 * Return: Always 1 
 */
 int main(void)
 {
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
