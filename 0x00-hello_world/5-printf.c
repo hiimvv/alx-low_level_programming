802492b59dda6a85c06f27f02410d9abf88414fe
hiimvv <hemo.hhh71@gmail.com>
2023-08-10 13:42:40 -0700

    prinf in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a statement using printf functions
+ * Return: 0 Success
+ */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
