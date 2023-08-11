6de36d22bc019bae158b29368e180b132eb60d5c
hiimvv <hemo.hhh71@gmail.com>
2023-08-11 16:40:54 -0700

    puts

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - A program that prints the alphabetic letters
  * Return: Alaways 0
@@ -6,7 +7,7 @@
 int main(void)
 {
 	char a;
-	for (a = "a" ; a >= "z" ; a++)
+	for (a = 'a' ; a <= 'z' ; a++)
 	putchar(a);
 	putchar("\n");
 	return (0);
