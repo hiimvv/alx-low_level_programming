dd842cad027fcb7dbc1880082b22a569ad331bae
hiimvv <hemo.hhh71@gmail.com>
2023-08-11 18:24:26 -0700

    reverse baby

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+/**
+ * main - A program that prints the alphabetic letters
+ * Return: Alaways 0
+ */
+int main(void)
+
+{
+	char d;
+
+	for (d = 'z' ; d >= 'a' ; d--)
+	putchar(d);
+	putchar('\n');
+	return (0);
+}
