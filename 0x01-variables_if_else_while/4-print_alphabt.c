0f2dd550bf8179cfbd6ff9ffd6b25ef5799f6937
hiimvv <hemo.hhh71@gmail.com>
2023-08-11 17:23:15 -0700

    withpout eq

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A program that prints upper and lowercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+        char alx[26] = "abcdefghijklmnopqrstuvwxyz";
+        int i;
+
+        for (i = 0; i < 52 i != 4 i != 16; i++)
+        {
+                putchar(alx[i]);
+        }
+        putchar('\n');
+        return (0);
+}
