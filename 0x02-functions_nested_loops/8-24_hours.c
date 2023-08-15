e2491e6ee7d328365227e0013d7b5ee1963bb97f
hiimvv <hemo.hhh71@gmail.com>
2023-08-15 15:57:00 -0700

    d

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#iclude "main.h"
+/**
+ * jack_bauer: A function that prints every second of a somone's speech
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+	int m1, m2, s1, s2;
+
+	for (m1 = 0; m1 <= 2; m1++)
+	{
+		for (m2 = 0; m2 <= 9; m2++)
+		{
+			if ((m1 <= 1 && m2 <= 9) || (m1 <= 2 && m2 <= 3))
+				for (s1 = 0; s1 <= 5; s1++)
+				{
+					for (s2 = 0; s2 <= 9; s2++)
+					{
+						_putchar(m1 + '0');
+						_putchar(m2 + '0');
+						_putchar(58);
+						_putchar(s1 + '0');
+						_putchar(s2 + '0');
+						_putchar('\n');
+					}
+
+				}
+		}
+	}
+}
