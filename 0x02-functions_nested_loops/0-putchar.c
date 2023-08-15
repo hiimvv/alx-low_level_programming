81a759c2962edee1ce9b30662616b02a8a9417bd
hiimvv <hemo.hhh71@gmail.com>
2023-08-15 02:54:27 -0700

    alpha

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - A program that prints the word "_putchar"
+ * Return: Always 1
+ */
+
+int main(void) 
+{ 
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+
