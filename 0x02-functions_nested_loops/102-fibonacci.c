88c3560db2e01f0e9657a2dcb1873d01a8d4dc3d
hiimvv <hemo.hhh71@gmail.com>
2023-08-16 00:46:22 -0700

    fibbo

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the first 50 fibbonaci sequenec numbers.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, n;
+	int t1 = 0, t2 = 1;
+	int nextTerm = t1 + t2;
+	int n = 50;
+
+	printf("%d, %d, ", t1, t2);
+	for (i = 3; i <= n; ++i)
+	{
+		printf("%d, ", nextTerm);
+		t1 = t2;
+		t2 = nextTerm;
+		nextTerm = t1 + t2;
+	}
+return (0);
+}
