255355139b3d48ca17cf707803cfa697bc16bc35
hiimvv <hemo.hhh71@gmail.com>
2023-09-25 13:24:45 -0700

    10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (i < index - 1)
+	while (n < index - 1)
 	{
 		if (!tempr || !(tempr->next))
 			return (-1);
